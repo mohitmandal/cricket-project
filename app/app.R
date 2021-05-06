80b5e365f5d43ad85289c340f653e0c39a45226f
Mohit Mandal <mohitmandal.24@gmail.com>
2021-05-06 17:28:40 -0400

    added interpretation of models

--- app/app.R
+++ app/app.R
@@ -7,7 +7,7 @@ ui <- navbarPage(
     tabPanel("Background",
              h5("How have Indian cricketers performed in T20 cricket since its inception in the early 2000s?"),
              p("Twenty-20, or T20, is the newest format of cricket, which was officially sanctioned by the International Council of Cricket (ICC) in 2004, with the first international men's T20 taking place between Australia and New Zealand in 2005."),
-             p("In a Twenty20 game, two teams have a single innings each, which is restricted to a maximum of 20 overs (or 120 balls). Since its inception, T20 cricket has soared in popularity, as is observable from the chart below, especially with the rise of competitive franchise leagues such as the Indian Premier League. The game is fast-paced and often relies upon batsmen scoring runs at a quick run-rate, as compared to the 50-over (ODI, i.e. One Day International) format."),
+             p("In a Twenty20 game, two teams have a single innings each, which is restricted to a maximum of 20 overs (or 120 balls). Since its inception, T20 cricket has soared in popularity, as is observable from the chart below, especially with the rise of competitive franchise leagues such as the Indian Premier League. The game is fast-paced and often relies upon batsmen scoring runs at a quick strike-rate, as compared to the 50-over (ODI, i.e. One Day International) format."),
              p("In this analysis, I seek to understand the evolution of T20 cricket in the past 15 years through a number of parameters."),
              
              # br() is a useful function. It adds white space to the page.
@@ -17,6 +17,8 @@ ui <- navbarPage(
     ),
     tabPanel("Batsman analysis",
              h4("How have top Indian players fared since their debut?"),
+             p("In this analysis, I examine runs scored by a batsman over the course of their entire career and their strike rate. Strike rate (SR) is calculated as runs scored per 100 balls faced, and provides a measure of the pace at which a batsman scores runs."),
+             br(),
              selectInput("batsman",
                          "Select a batsman",
                          c("Virat Kohli", "Rohit Sharma", "Shikhar Dhawan", "KL Rahul")
@@ -37,11 +39,15 @@ ui <- navbarPage(
     tabPanel("Predicting Future Performance", 
              h3("What is India's best opening combination?"),
              p("In 2021 and 2022, there are two T20 World Cups scheduled. Based on their performances to-date, can we evaluate India's best openers? What is India's best opening combination?"),
+             p("These models project average runs scored as a function of balls faced and batting position."),
              br(),
              plotOutput("plot_1"),
              br(),
-             p(" "),
-             plotOutput("plot_2")
+             p("The plot above predicts that KL Rahul and Rohit Sharma are India's best opening batsman based on prior performance. While the batsman are within a close range of values, between 51 to 54 runs scored, a 3 run difference in average is not insignificant. T20 games are often decided by 1 run or two."),
+             br(),
+             plotOutput("plot_2"),
+             br(),
+             p("This plot projects that India should opt for the opening combination of Rahul and Sharma for their best chance of success in the upcoming World Cups. As we can see, the opening combinations are again within a close range of values (from 103 to 106), which is actually indicative of how well these four batsman have performed in relation to one another."),
     ),
     tabPanel("About",
              h3("About Me"),
@@ -51,14 +57,16 @@ ui <- navbarPage(
              h3("About this Project"),
              p("I have always followed cricket, and cricket is very much a numbers-obsessed sport. So, it was natural that I would combine my love for cricket with my newfound data analysis skills to glean new insights about the sport."),
              h3("Data sources"),
-             p(tags$a(href="https://stats.espncricinfo.com/ci/engine/stats/index.html", "ESPNCricinfo"), "provided all of the data of this project. Specifically, I am indebted to the 'cricketr' package developed by Tinniam V Ganesh, which made it exceedingly easy to scrape data for players and teams from ESPNCricinfo."),
+             p(tags$a(href="https://stats.espncricinfo.com/ci/engine/stats/index.html", "ESPNCricinfo"), "provided all of the data of this project. Specifically, I am indebted to the", tags$a(href="https://cran.r-project.org/web/packages/cricketr/index.html", "cricketr"), "package developed by Tinniam V Ganesh, which made it exceedingly easy to scrape data for players and teams from ESPNCricinfo."),
              p("You can find my Github repository", tags$a(href="https://github.com/mohitmandal/cricket-project", "here."))))
 
 server <- function(input, output) {
     
     # These code chunks takes the input selection from the user and returns the
     # relevant plot. It's a tidy way of generating the plots on the cloud
-    # without needing to save them as images beforehand.
+    # without needing to save them as images beforehand. The advantage of this
+    # method is that the code scrapes and analyses the data each time, meaning
+    # that the plots should automatically update after each new cricket game.
     
     output$runs_plot <- renderPlot(
         if (input$batsman == "Virat Kohli") {
