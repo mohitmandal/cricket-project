8e979462b625022fbf96c7a33fc7bc2624da9bbe
Mohit Mandal <mohitmandal.24@gmail.com>
2021-05-07 14:03:21 -0400

    added interpretation

--- app/app.R
+++ app/app.R
@@ -7,7 +7,8 @@ ui <- navbarPage(
     tabPanel("Background",
              h5("How have Indian cricketers performed in T20 cricket since its inception in the early 2000s?"),
              p("Twenty-20, or T20, is the newest format of cricket, which was officially sanctioned by the International Council of Cricket (ICC) in 2004, with the first international men's T20 taking place between Australia and New Zealand in 2005."),
-             p("In a Twenty20 game, two teams have a single innings each, which is restricted to a maximum of 20 overs (or 120 balls). Since its inception, T20 cricket has soared in popularity, as is observable from the chart below, especially with the rise of competitive franchise leagues such as the Indian Premier League. The game is fast-paced and often relies upon batsmen scoring runs at a quick strike-rate, as compared to the 50-over (ODI, i.e. One Day International) format."),
+             p("In a Twenty20 game, two teams have a single innings each, which is restricted to a maximum of 20 overs (or 120 balls) or 10 wickets (or outs), whichever elapses first. The team that wins is the team that scores a minimum of 1 run more than the opposition."),
+             p("Since its inception, T20 cricket has soared in popularity, as is observable from the chart below, especially with the rise of competitive franchise leagues such as the Indian Premier League. The game is fast-paced and often relies upon batsmen scoring runs at a quick strike-rate, as compared to the 50-over (ODI, i.e. One Day International) format."),
              p("In this analysis, I seek to understand the evolution of T20 cricket in the past 15 years through a number of parameters."),
              
              # br() is a useful function. It adds white space to the page.
@@ -18,6 +19,7 @@ ui <- navbarPage(
     tabPanel("Batsman analysis",
              h4("How have top Indian players fared since their debut?"),
              p("In this analysis, I examine runs scored by a batsman over the course of their entire career and their strike rate. Strike rate (SR) is calculated as runs scored per 100 balls faced, and provides a measure of the pace at which a batsman scores runs."),
+             p("The increasing tendency of strike-rate indicates that each batsman has become more aggressive over the course of their careers, with the notable exception of KL Rahul, who started out with a high strike-rate (around 140) and has maintained it since."),
              br(),
              selectInput("batsman",
                          "Select a batsman",
@@ -48,16 +50,18 @@ ui <- navbarPage(
              plotOutput("plot_2"),
              br(),
              p("This plot projects that India should opt for the opening combination of Rahul and Sharma for their best chance of success in the upcoming World Cups. As we can see, the opening combinations are again within a close range of values (from 103 to 106), which is actually indicative of how well these four batsman have performed in relation to one another."),
+             br(),
+             br(),
     ),
     tabPanel("About",
              h3("About Me"),
-             p("My name is Mohit Mandal and I am a PhD student in the Department of Anthropology at 
-             Harvard  University. 
-             You can reach me at mandal@g.harvard.edu."),
+             p("My name is Mohit Mandal, I am a PhD student in the Department of Anthropology at 
+             Harvard  University."),
+             p("You can reach me at mandal@g.harvard.edu."),
              h3("About this Project"),
              p("I have always followed cricket, and cricket is very much a numbers-obsessed sport. So, it was natural that I would combine my love for cricket with my newfound data analysis skills to glean new insights about the sport."),
              h3("Data sources"),
-             p(tags$a(href="https://stats.espncricinfo.com/ci/engine/stats/index.html", "ESPNCricinfo"), "provided all of the data of this project. Specifically, I am indebted to the", tags$a(href="https://cran.r-project.org/web/packages/cricketr/index.html", "cricketr"), "package developed by Tinniam V Ganesh, which made it exceedingly easy to scrape data for players and teams from ESPNCricinfo."),
+             p(tags$a(href="https://stats.espncricinfo.com/ci/engine/stats/index.html", "ESPNCricinfo"), "provided all of the data on players and teams. Specifically, I am indebted to the", tags$a(href="https://cran.r-project.org/web/packages/cricketr/index.html", "cricketr"), "package developed by Tinniam V Ganesh, which made it exceedingly easy to scrape this data from ESPNCricinfo."),
              p("You can find my Github repository", tags$a(href="https://github.com/mohitmandal/cricket-project", "here."))))
 
 server <- function(input, output) {
